9fca3a252d9f64b53d3e18315c1e12c66c2c46c0
Erik Gomez <e@eriknicolasgomez.com>
2021-10-21 16:31:07 -0500

    add monterey sucatalog (#99)

--- installinstallmacos.py
+++ installinstallmacos.py
@@ -57,6 +57,9 @@ DEFAULT_SUCATALOGS = {
     '20': 'https://swscan.apple.com/content/catalogs/others/'
           'index-11-10.15-10.14-10.13-10.12-10.11-10.10-10.9'
           '-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog',
+    '21': 'https://swscan.apple.com/content/catalogs/others/'
+          'index-12-10.16-10.15-10.14-10.13-10.12-10.11-10.10-10.9'
+          '-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog',
 }
 
 SEED_CATALOGS_PLIST = (
