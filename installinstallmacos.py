e2d5ee2e2cd4d8ab6a8a830780e878c1981768a2
Graham Pugh <g.r.pugh@gmail.com>
2021-10-24 16:14:42 +0200

    add macOS 12 catalog

--- installinstallmacos.py
+++ installinstallmacos.py
@@ -60,7 +60,7 @@ DEFAULT_SUCATALOGS = {
     "index-10.16-10.15-10.14-10.13-10.12-10.11-10.10-10.9"
     "-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog",
     "21": "https://swscan.apple.com/content/catalogs/others/"
-    "index-10.16-10.15-10.14-10.13-10.12-10.11-10.10-10.9"
+    "index-12-10.16-10.15-10.14-10.13-10.12-10.11-10.10-10.9"
     "-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog",
 }
 
