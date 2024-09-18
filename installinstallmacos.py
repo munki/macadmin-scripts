77e507e4351bd35db771706c4f3d6955b6bce968
Greg Neagle <Gregneagle@mac.com>
2024-09-18 08:45:38 -0700

    Add Sequoia sucatalog URL

--- installinstallmacos.py
+++ installinstallmacos.py
@@ -71,6 +71,9 @@ DEFAULT_SUCATALOGS = {
           '-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog',
     '23': 'https://swscan.apple.com/content/catalogs/others/'
           'index-14-13-12-10.16-10.15-10.14-10.13-10.12-10.11-10.10-10.9'
+          '-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog',
+    '24': 'https://swscan.apple.com/content/catalogs/others/'
+          'index-15-14-13-12-10.16-10.15-10.14-10.13-10.12-10.11-10.10-10.9'
           '-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog'
 }
 
